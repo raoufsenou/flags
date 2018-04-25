599d0f340dc79a3cd7727da4a53559b362a09f3b
raoufsenou <raoufsenou@gmail.com>
2018-04-25 21:04:07 +0100

    add doc.go file

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package flags is a set of costum defined flags that i need
+// for my own project and which can be easily used by the "flag" 
+// package from the standard library.
+package flags
